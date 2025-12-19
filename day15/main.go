e19021634d6de8ed00857fa4513c2515414e5a3b
Kevin Ho <kevinkh08@gmail.com>
2025-12-18 20:37:00 -0800

    day15 boilerplate

---
+++ day15/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"aoc-2015/common"
+	"bytes"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+type Ingredient struct {
+	Name       string
+	Capacity   int
+	Durability int
+	Flavor     int
+	Texture    int
+	Calories   int
+}
+
+func parseIngredients(data []byte) ([]Ingredient, error) {
+
+	var res []Ingredient
+
+	for entry := range bytes.SplitSeq(data, []byte{10}) {
+
+		entryStr := strings.Split(string(entry), " ")
+
+		name := entryStr[0]
+		name, _ = strings.CutSuffix(name, ":")
+
+		capacity := entryStr[2]
+		capacity, _ = strings.CutSuffix(capacity, ",")
+		capacityInt, err := strconv.Atoi(capacity)
+		if err != nil {
+			return res, err
+		}
+
+		durability := entryStr[4]
+		durability, _ = strings.CutSuffix(durability, ",")
+		durabilityInt, err := strconv.Atoi(durability)
+		if err != nil {
+			return res, err
+		}
+
+		flavor := entryStr[6]
+		flavor, _ = strings.CutSuffix(flavor, ",")
+		flavorInt, err := strconv.Atoi(flavor)
+		if err != nil {
+			return res, err
+		}
+
+		texture := entryStr[8]
+		texture, _ = strings.CutSuffix(texture, ",")
+		textureInt, err := strconv.Atoi(texture)
+		if err != nil {
+			return res, err
+		}
+
+		calories := entryStr[10]
+		caloriesInt, err := strconv.Atoi(calories)
+		if err != nil {
+			return res, err
+		}
+
+		res = append(res, Ingredient{
+			Name:       name,
+			Capacity:   capacityInt,
+			Durability: durabilityInt,
+			Flavor:     flavorInt,
+			Texture:    textureInt,
+			Calories:   caloriesInt,
+		})
+
+	}
+
+	return res, nil
+
+}
+
+func main() {
+	filePath := "./inputExample.txt"
+	data, err := common.ReadInput(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data = common.TrimNewLineSuffix(data)
+
+	ingredients, err := parseIngredients(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ingredients)
+
+}
